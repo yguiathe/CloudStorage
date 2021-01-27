65a14c8faebaa6aef6e34928a90ac500e8b0e008
yguiathe <tectoo@Yvans-MacBook-Pro.local>
2021-01-27 01:19:26 -0800

    JUnit/Selenium Tests

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,41 +1,251 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FilenameUtils;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import static java.lang.Thread.sleep;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CloudStorageApplicationTests {
 
 	@LocalServerPort
 	private int port;
 
-	private WebDriver driver;
+	private static WebDriver driver;
+
+	public String baseURL;
+
+	private static final String FIRSTNAME = "Vanessa";
+	private static final String LASTNAME = "Carlton";
+	private static final String USERNAME = "vcarlton";
+	private static final String PASSWORD = "100@L-23ds";
+	private static final String HOME_PAGE_TITLE = "Home";
+	private static final String SIGNUP_PAGE_TITLE = "Sign Up";
+	private static final String LOGIN_PAGE_TITLE = "Login";
+
+	// Notes
+	private static final String FIRST_NOTE_TITLE = "My First Note";
+	private static final String FIRST_NOTE_DESCRIPTION = "My First Note Description";
+	private static final String SECOND_NOTE_TITLE = "My Second Note";
+	private static final String SECOND_NOTE_DESCRIPTION = "My Second Note Description";
+	private static final String THIRD_NOTE_TITLE = "My Third Note";
+	private static final String THIRD_NOTE_DESCRIPTION = "My Third Note Description";
+	private static final String UPDATED_NOTE_TITLE = "My Third Note Updt";
+	private static final String UPDATED_NOTE_DESCRIPTION = "My Third Note Description (updated)";
+
+	// Credentials
+	private static final String CRED_URL_A = "https://m.facebook.com/";
+	private static final String CRED_USERNAME_A = "masterkg";
+	private static final String CRED_PASSWORD_A = "h54_32kr";
+	private static final String UPDATED_USERNAME = "omarsy";
+	private static final String UPDATED_PASSWORD = "rR33_p0s";
+	private static final String CRED_URL_B = "https://m.gmail.com/";
+	private static final String CRED_USERNAME_B = "vcarlton";
+	private static final String CRED_PASSWORD_B = "ro0ro0T_";
+	private static final String CRED_URL_C = "https://classroom.udacity.com/";
+	private static final String CRED_USERNAME_C = "ssabri";
+	private static final String CRED_PASSWORD_C = "r@viol1";
+
+	// Files
+	private static final String FILENAME_A = "/Users/tectoo/Downloads/output_15.pdf";
+	private static final String FILENAME_B = "/Users/tectoo/Downloads/nd-term-invoice.pdf";
 
 	@BeforeAll
-	static void beforeAll() {
+	public static void beforeAll() {
 		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+	}
+
+	@AfterAll
+	public static void afterAll() {
+		driver.quit();
+		driver = null;
 	}
 
 	@BeforeEach
 	public void beforeEach() {
-		this.driver = new ChromeDriver();
-	}
-
-	@AfterEach
-	public void afterEach() {
-		if (this.driver != null) {
-			driver.quit();
-		}
+		baseURL = baseURL = "http://localhost:" + port;
 	}
 
 	@Test
-	public void getLoginPage() {
-		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
+	@Order(1)
+	public void loginPage() {
+		driver.get(baseURL + "/login");
+		Assertions.assertEquals(LOGIN_PAGE_TITLE, driver.getTitle());
+	}
+
+	@Test
+	@Order(2)
+	public void successfulSignupAndLogin() throws InterruptedException {
+
+		driver.get(baseURL + "/signup");
+
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(FIRSTNAME, LASTNAME, USERNAME, PASSWORD);
+
+		sleep(4000);
+
+		Assertions.assertEquals(LOGIN_PAGE_TITLE, driver.getTitle());
+
+		driver.get(baseURL + "/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(USERNAME, PASSWORD);
+
+		Assertions.assertEquals(HOME_PAGE_TITLE, driver.getTitle());
+
+	}
+
+	@Test
+	@Order(3)
+	public void unSuccessfulSignupGivenDuplicateUsername() {
+
+		driver.get(baseURL + "/signup");
+
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(FIRSTNAME, LASTNAME, USERNAME, PASSWORD);
+
+		Assertions.assertEquals(SIGNUP_PAGE_TITLE, driver.getTitle());
+
+	}
+
+	@Test
+	@Order(4)
+	public void successfulFileUpload() throws InterruptedException {
+		driver.get(baseURL + "/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(USERNAME, PASSWORD);
+
+		sleep(5000);
+		HomePage homePage = new HomePage(driver);
+		homePage.uploadFile(FILENAME_A);
+		sleep(5000);
+
+		Assertions.assertEquals(true, homePage.fileExistsInList(FilenameUtils.getName(FILENAME_A)));
+
+	}
+
+	@Test
+	@Order(5)
+	public void successfulFileDeletion() throws InterruptedException {
+
+		HomePage homePage = new HomePage(driver);
+		homePage.uploadFile(FILENAME_B);
+		sleep(5000);
+		homePage.deleteFile(0);
+		sleep(5000);
+
+		Assertions.assertEquals(false, homePage.fileExistsInList(FILENAME_A));
+
+	}
+
+	@Test
+	@Order(6)
+	public void successfulNoteCreation() throws InterruptedException {
+
+		driver.get(baseURL + "/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(USERNAME, PASSWORD);
+
+		HomePage homePage = new HomePage(driver);
+		homePage.addNote(FIRST_NOTE_TITLE, FIRST_NOTE_DESCRIPTION);
+		sleep(5000);
+
+		Assertions.assertEquals(true, homePage.noteExistsInList(FIRST_NOTE_TITLE, FIRST_NOTE_DESCRIPTION));
+
+	}
+
+	@Test
+	@Order(7)
+	public void successfulNoteUpdate() throws InterruptedException {
+		HomePage homePage = new HomePage(driver);
+
+		// Adding notes
+		homePage.addNote(SECOND_NOTE_TITLE, SECOND_NOTE_DESCRIPTION);
+		sleep(5000);
+		homePage.addNote(THIRD_NOTE_TITLE, THIRD_NOTE_DESCRIPTION);
+		sleep(5000);
+		homePage.openEditNoteModal(2);
+		sleep(5000);
+		Assertions.assertEquals(true, homePage.noteModalHasCorrectValues(THIRD_NOTE_TITLE, THIRD_NOTE_DESCRIPTION));
+		sleep(5000);
+
+		// Editing third note
+		homePage.editNote(UPDATED_NOTE_TITLE, UPDATED_NOTE_DESCRIPTION);
+		sleep(5000);
+
+		Assertions.assertEquals(true, homePage.noteExistsInList(UPDATED_NOTE_TITLE, UPDATED_NOTE_DESCRIPTION));
+	}
+
+	@Test
+	@Order(8)
+	public void successfulNoteDeletion() throws InterruptedException {
+
+		HomePage homePage = new HomePage(driver);
+		homePage.deleteNote(0);
+		sleep(5000);
+
+		Assertions.assertEquals(false, homePage.noteExistsInList(FIRST_NOTE_TITLE, FIRST_NOTE_DESCRIPTION));
+
+	}
+
+	@Test
+	@Order(9)
+	public void successfulCredentialCreation() throws InterruptedException {
+
+		driver.get(baseURL + "/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(USERNAME, PASSWORD);
+
+		HomePage homePage = new HomePage(driver);
+		sleep(5000);
+		homePage.addCredential(CRED_URL_A, CRED_USERNAME_A, CRED_PASSWORD_A);
+		homePage.addCredential(CRED_URL_B, CRED_USERNAME_B, CRED_PASSWORD_B);
+		homePage.addCredential(CRED_URL_C, CRED_USERNAME_C, CRED_PASSWORD_C);
+		sleep(5000);
+
+		Assertions.assertEquals(true, homePage.credentialExistsInList(CRED_URL_B, CRED_USERNAME_B, CRED_PASSWORD_B));
+		Assertions.assertEquals(true, homePage.credentialExistsInList(CRED_URL_A, CRED_USERNAME_A, CRED_PASSWORD_A));
+		Assertions.assertEquals(true, homePage.credentialExistsInList(CRED_URL_C, CRED_USERNAME_C, CRED_PASSWORD_C));
+
+	}
+
+	@Test
+	@Order(10)
+	public void successfulCredentialViewAndUpdate() throws InterruptedException {
+		HomePage homePage = new HomePage(driver);
+
+		homePage.openEditCredentialModal(0);
+		sleep(5000);
+		Assertions.assertEquals(true, homePage.credentialModalHasCorrectValues(CRED_URL_A, CRED_USERNAME_A, CRED_PASSWORD_A));
+		sleep(5000);
+
+		homePage.editCredential(CRED_URL_A, UPDATED_USERNAME, UPDATED_PASSWORD);
+		sleep(5000);
+
+		Assertions.assertEquals(true, homePage.credentialExistsInList(CRED_URL_A, UPDATED_USERNAME, UPDATED_PASSWORD));
+	}
+
+	@Test
+	@Order(11)
+	public void successfulCredentialDeletion() throws InterruptedException {
+
+		HomePage homePage = new HomePage(driver);
+		homePage.deleteCredential(1);
+		sleep(5000);
+
+		Assertions.assertEquals(false, homePage.credentialExistsInList(CRED_URL_B, CRED_USERNAME_B, CRED_PASSWORD_B));
+
 	}
 
 }
--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
@@ -1,23 +1,57 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static java.lang.Thread.sleep;
 
 public class HomePage {
 
-    @FindBy(css = "#inputFirstName")
-    private WebElement firstNameField;
+    private WebDriver webDriver;
 
-    @FindBy(css = "#inputLastName")
-    private WebElement lastNameField;
+    @FindBy(css = "#openNoteModal")
+    private WebElement noteModalButton;
 
-    @FindBy(css = "#inputUsername")
-    private WebElement usernameField;
+    @FindBy(css = "#notesList")
+    private WebElement notesTable;
 
-    @FindBy(css = "#inputPassword")
-    private WebElement passwordField;
+    @FindBy(css = "#credentialsList")
+    private WebElement credentialsTable;
+
+    @FindBy(css = "#filesList")
+    private WebElement filesTable;
+
+    @FindBy(css = "#openCredentialModal")
+    private WebElement credentialModalButton;
+
+    @FindBy(css = "#note-title")
+    private WebElement noteTitleField;
+
+    @FindBy(css = "#note-description")
+    private WebElement noteDescriptionField;
+
+    @FindBy(css = "#saveNote")
+    private WebElement saveNoteButton;
+
+    @FindBy(css = "#credential-url")
+    private WebElement credUrlField;
+
+    @FindBy(css = "#credential-username")
+    private WebElement credUnameField;
+
+    @FindBy(css = "#credential-password")
+    private WebElement credPassField;
+
+    @FindBy(css = "#saveCredential")
+    private WebElement saveCredentialButton;
 
     @FindBy(css = "#logoutBtn")
     private WebElement logoutButton;
@@ -28,17 +62,165 @@ public class HomePage {
     @FindBy(css = "#uploadBtn")
     private WebElement uploadButton;
 
+    @FindBy(css = "#nav-files-tab")
+    private WebElement filesTabButton;
+
+    @FindBy(css = "#nav-notes-tab")
+    private WebElement notesTabButton;
+
+    @FindBy(css = "#nav-credentials-tab")
+    private WebElement credentialsTabButton;
+
+    @FindBy(css = ".delete-note")
+    private List<WebElement> deleteNoteButtons;
+
+    @FindBy(css = ".delete-file")
+    private List<WebElement> deleteFileButtons;
+
+    @FindBy(css = ".delete-credential")
+    private List<WebElement> deleteCredentialButtons;
+
+    @FindBy(css = ".edit-note")
+    private List<WebElement> editNoteButtons;
+
+    @FindBy(css = ".view-file")
+    private List<WebElement> viewFileButtons;
+
+    @FindBy(css = ".edit-credential")
+    private List<WebElement> editCredentialButtons;
+
+    private JavascriptExecutor jse;
+
     public HomePage(WebDriver webDriver) {
+        this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this);
+        this.jse = (JavascriptExecutor) webDriver;
+    }
+
+    private void waitForVisibility(WebElement element) throws Error{
+        new WebDriverWait(webDriver, 60)
+                .until(ExpectedConditions.visibilityOf(element));
     }
 
     public void logout() {
         this.logoutButton.click();
     }
 
-    public void uploadFile(String filename) {
+    public void uploadFile(String filename) throws InterruptedException {
         this.fileUpload.sendKeys(filename);
         this.uploadButton.click();
+        sleep(5000);
+    }
+
+    public void addNote(String noteTitle, String noteDescription) throws InterruptedException {
+        this.jse.executeScript("arguments[0].click()", this.notesTabButton);
+        sleep(5000);
+        this.jse.executeScript("arguments[0].click()", this.noteModalButton);
+        sleep(5000);
+        this.noteTitleField.sendKeys(noteTitle);
+        this.noteDescriptionField.sendKeys(noteDescription);
+        this.saveNoteButton.click();
+    }
+
+    public void addCredential(String url, String username, String password) throws InterruptedException {
+        this.jse.executeScript("arguments[0].click()", this.credentialsTabButton);
+        sleep(5000);
+        this.jse.executeScript("arguments[0].click()", this.credentialModalButton);
+        sleep(5000);
+        this.credUrlField.sendKeys(url);
+        this.credUnameField.sendKeys(username);
+        this.credPassField.sendKeys(password);
+        this.saveCredentialButton.click();
+    }
+
+    public void openEditNoteModal(int index) {
+        this.editNoteButtons.get(index).click();
+    }
+
+    public void editNote(String noteTitle, String noteDescription) {
+        this.noteTitleField.clear();
+        this.noteTitleField.sendKeys(noteTitle);
+        this.noteDescriptionField.clear();
+        this.noteDescriptionField.sendKeys(noteDescription);
+        this.saveNoteButton.click();
+    }
+
+    public void openEditCredentialModal(int index) {
+        this.editCredentialButtons.get(index).click();
+    }
+
+    public void editCredential(String url, String username, String password) {
+        this.credUrlField.clear();
+        this.credUnameField.clear();
+        this.credPassField.clear();
+        this.credUrlField.sendKeys(url);
+        this.credUnameField.sendKeys(username);
+        this.credPassField.sendKeys(password);
+        this.saveCredentialButton.click();
+    }
+
+    public boolean noteModalHasCorrectValues(String noteTitle, String noteDescription) {
+        return (this.noteTitleField.getAttribute("value").equals(noteTitle) && this.noteDescriptionField.getAttribute("value").equals(noteDescription));
+    }
+
+    public boolean credentialModalHasCorrectValues(String url, String username, String password) {
+        return (this.credUrlField.getAttribute("value").equals(url) && this.credUnameField.getAttribute("value").equals(username) && this.credPassField.getAttribute("value").equals(password));
+    }
+
+    public void deleteNote(int index) throws InterruptedException {
+        this.jse.executeScript("arguments[0].click()", this.notesTabButton);
+        sleep(5000);
+        this.deleteNoteButtons.get(index).click();
+    }
+
+    public void deleteCredential(int index) throws InterruptedException {
+        this.jse.executeScript("arguments[0].click()", this.credentialsTabButton);
+        sleep(5000);
+        this.deleteCredentialButtons.get(index).click();
+    }
+
+    public void deleteFile(int index) {
+        this.filesTabButton.click();
+        this.deleteFileButtons.get(index).click();
+    }
+
+    public boolean noteExistsInList(String noteTitle, String noteDescription) {
+        boolean flag = false;
+        List<WebElement> notes = this.notesTable.findElements(By.tagName("tr"));
+        for(WebElement note: notes) {
+            if(note.findElement(By.tagName("th")).getText().equals(noteTitle) && note.findElement(By.xpath("td[2]")).getText().equals(noteDescription)) {
+                flag = true;
+                break;
+            }
+        }
+
+        return flag;
+    }
+
+    public boolean credentialExistsInList(String url, String username, String password) {
+        boolean flag = false;
+        List<WebElement> credentials = this.credentialsTable.findElements(By.tagName("tr"));
+        for(WebElement credential: credentials) {
+            if(credential.findElement(By.tagName("th")).getText().equals(url) && credential.findElement(By.xpath("td[2]")).getText().equals(username) && !credential.findElement(By.xpath("td[3]")).getText().equals(password)) {
+                flag = true;
+                break;
+            }
+        }
+
+        return flag;
+    }
+
+    public boolean fileExistsInList(String filename) {
+        boolean flag = false;
+        List<WebElement> files = this.filesTable.findElements(By.tagName("tr"));
+        for(WebElement file: files) {
+            if(file.findElement(By.tagName("th")).getText().equals(filename)) {
+                flag = true;
+                break;
+            }
+        }
+
+        return flag;
     }
 
 }
