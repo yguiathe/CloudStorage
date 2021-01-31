fb0ff90a87fc1b64afda52dc9f1024945c743976
yguiathe <tectoo@Yvans-MacBook-Pro.local>
2021-01-30 19:37:08 -0800

    Adds server.tomcat.max-swallow-size to application.properties file and sets it -1 to allow large files upload.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/interceptor/FileUploadExceptionAdvice.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/interceptor/FileUploadExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage.interceptor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
@@ -11,10 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 @ControllerAdvice
 public class FileUploadExceptionAdvice {
 
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxFileSize;
+
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ModelAndView handleMaxSizeException(MaxUploadSizeExceededException exc, HttpServletRequest request, HttpServletResponse response){
         ModelAndView modelAndView = new ModelAndView("home");
-        modelAndView.getModel().put("uploadError", "File size is too large - it shouldn't exceed 5MB!");
+        modelAndView.getModel().put("uploadError", "File size is too large - it shouldn't exceed " + maxFileSize + "!");
         return modelAndView;
     }
 }
